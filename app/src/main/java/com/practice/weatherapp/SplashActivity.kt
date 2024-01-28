71787f78072a4ad8cd825a2944bbc0bd998052d7
YashNagare <yashpnagare@gmail.com>
2024-01-28 23:31:03 +0530

    Changed splash activity time from 4s to 3s

--- app/src/main/java/com/practice/weatherapp/SplashActivity.kt
+++ app/src/main/java/com/practice/weatherapp/SplashActivity.kt
@@ -15,6 +15,6 @@ class SplashActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        },4000)
+        },3000)
     }
 }
